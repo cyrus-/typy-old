bf1cf7f65b16c9bd1580dd7e56c923770a0323f9
Cyrus Omar <cyology@gmail.com>
2015-09-24 14:12:28 -0400

    Added mk_unit to test_std

--- tests/test_std.py
+++ tests/test_std.py
@@ -6,3 +6,14 @@ To run:
 import typy
 import typy.std as std
 
+@std.fn
+def mk_unit():
+    """This is a docstring."""
+    {} >> std.unit
+    return ()
+
+# TODO: test docstring
+# TODO: test type annotation system
+# TODO: test unit intro
+# TODO: test return
+# TODO: test code generation
