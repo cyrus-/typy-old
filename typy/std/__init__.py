3b3ff54191e5c78cf5731779dd3dcbb4a528a8bd
Cyrus Omar <cyology@gmail.com>
2015-09-24 14:06:55 -0400

    Added some TODOs to std

--- typy/std/__init__.py
+++ typy/std/__init__.py
@@ -10,4 +10,20 @@ class unit_(typy.Type):
     # TODO: intro operator
 unit = unit_[()]
 
+class fn(typy.Type):
+    @classmethod
+    def validate_idx(cls, idx):
+        # TODO: validate idx
+        pass
 
+    @classmethod
+    def validate_inc_idx(cls, inc_idx):
+        # TODO: validate inc indx
+        pass
+
+    # TODO: rest of the methods
+
+# TODO: tuples and ltuples
+# TODO: casetypes
+# TODO: string
+# TODO: number
